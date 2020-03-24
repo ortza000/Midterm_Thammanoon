545695f924de68abfc241433b2acf156d6dadd68
ortza000 <59310369@go.buu.ac.th>
2020-03-24 09:53:24 +0700

    เพื่มลิ้ง Youtube

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <?php echo "รหัสนิสิต-59310369"; ?><br>
 <?php echo "ชื่อ-สกุล นาย ธรรมนูญ จำรัส"; ?><br>
 <iframe width="500" height="500"
-src="https://www.youtube.com/embed/RgKAFK5djSk?autoplay=1">
+src="https://www.youtube.com/embed/TKYXW6CT_30">
 </iframe>
 </BODY>
 </HTML>
